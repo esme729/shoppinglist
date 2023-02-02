959f1e34b466e8aa78375a004a58092b71b77d19
Esmay <Molinaesme729@gmail.com>
2023-02-02 10:44:33 -0700

    added items

---
+++ Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  shoppinglist
+//
+//  Created by Molina, Yadira - Student on 1/26/23.
+//
+
+import Foundation
+class Item
+
+{
+    var name:String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+    
--- shoppinglist/ViewController.swift
+++ shoppinglist/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tablView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tablView.dequeueReusableCell(withIdentifier: "celid", for: indexPath)
+        cell.textLabel?.text = "hello"
+      return cell
+    }
+    
+    var item : [Item] = []
+    @IBOutlet weak var tablView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tablView.dataSource = self
+    let Item1 = Item(name:"grapes")
+    let Item2 = Item(name:"water")
+        item = [Item1,Item2]
+}
+    func tableView(_ tablView: UITableView,numberOfRowsInSection section:Int ) -> Int
+    {
+        return item.count
+        
+        
     }
-
+      
+//        func tableView(_tableView: UITableView, cellFowRowAt inexPath)->
+//    }
+//
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
